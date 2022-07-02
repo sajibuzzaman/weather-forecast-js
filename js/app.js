3a8549bbf7512b6dfaa2c4994145ae2771578cb1
Sajib Uzzaman <muhammadsajibuzzaman1998@gmail.com>
2022-07-02 22:58:41 +0600

    Add spinner and api link modify

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
+const API_KEY = '005f235fb8f211ddba4217fd7c8a6ede';
+
+// Show Spinner Function 
+const showSpinner = (displayValue) => {
+    document.getElementById('spinner').style.display = displayValue;
+}
+
 const getInput = () => {
     const inputCity = document.getElementById('input-city');
     const inputCityValue = inputCity.value;
     loadWeather(inputCityValue);
     inputCity.value = '';
+    document.getElementById('display').textContent = '';
+    showSpinner('block');
 }
 
 const loadWeather = async (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=005f235fb8f211ddba4217fd7c8a6ede`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
     displayWeather(data);
@@ -14,24 +23,28 @@ const loadWeather = async (city) => {
 
 const displayWeather = (data) => {
     if (data.cod == 200) {
-        temparatur = parseInt(data.main.temp - 273.15);
-    const displayContainer = document.getElementById('display');
-    displayContainer.textContent = '';
-    const div = document.createElement('div');
-    div.innerHTML = `
+        // temparatur = parseInt(data.main.temp - 273.15);
+        const displayContainer = document.getElementById('display');
+        // displayContainer.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
     <div class="card mb-3 mx-auto bg-info bg-opacity-50" style="width: 20rem;">
-        <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" class="card-img-top rounded mx-auto" style="height: 10rem; width: 10rem;" alt="...">
+       <!-- <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" 
+        class="card-img-top rounded mx-auto" style="height: 10rem; width: 10rem;" alt="..."> -->
+
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" 
+        class="card-img-top rounded mx-auto" style="height: 10rem; width: 10rem;" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">${data.name}</h5>
-            <p class="card-text text-center">${temparatur}° C</p>
+            <p class="card-text text-center">${parseInt(data.main.temp)}° C</p>
             <p class="card-text text-center">${data.weather[0].main}</p>
         </div>
     </div>
     `;
-    displayContainer.appendChild(div);
-    alert('City Added Succefully');
+        displayContainer.appendChild(div);
+        alert('City Added Succefully');
     } else {
         alert('Please Enter Valid City');
     }
-    
+    showSpinner('none');
 }
